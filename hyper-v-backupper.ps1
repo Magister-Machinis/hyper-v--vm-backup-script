3aa38193288c99f51889c43dc0f00c1aacbb3ce0
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2021-01-24 21:23:21 -0500

    testing complete

--- hyper-v-backupper.ps1
+++ hyper-v-backupper.ps1
@@ -2,24 +2,25 @@
 param(
 [parameter(mandatory=$true)][string]$destination=$(throw "enter destination folder"),
 [parameter(mandatory=$true)][int]$backupdepth=$(throw "enter number of backups to keep"),
-[string]$password=$null
+[string]$password=$null,
+[string]$intermediarydir=$env:TEMP
 )
 
 $name = Get-Date -format "yyyy-MM-dd-HH-mm"
 
-Get-VM | ForEach-Object {Export-VM -Name $_.Name -Path "$env:TEMP\$name" -CaptureLiveState CaptureSavedState -AsJob} | Wait-Job
+Get-VM | ForEach-Object {Export-VM -Name $_.Name -Path "$intermediarydir\$name" -CaptureLiveState CaptureSavedState -AsJob} | Wait-Job
 
 if ([string]::IsNullOrEmpty($password))
 {
-	"'C:\Program Files\7-Zip\7z.exe' a -mx=9 -sdel '$destination\$name.7z' '$env:TEMP\$name'"
+	& "$env:ProgramFiles\7-Zip\7z.exe" a -mx=9 -sdel "$destination\$name.7z" "$intermediarydir\$name"
 }
 else
 {
-	"'C:\Program Files\7-Zip\7z.exe' a -mx=9 -p='$password' -sdel '$destination\$name.7z' '$env:TEMP\$name'"
+	& "$env:ProgramFiles\7-Zip\7z.exe" a -mx=9 -p="$password" -sdel "$destination\$name.7z" "$intermediarydir\$name"
 }
 
 
-while ((Get-ChildItem "$destination" -filter *.7z | measure-object).Count > $backupdepth)
+while ((Get-ChildItem "$destination" -filter *.7z | measure-object).Count -gt $backupdepth)
 {
 	Get-ChildItem -Path "$destination" -filter *.7z | sort creationtime | select -First 1 | Remove-Item
 }
